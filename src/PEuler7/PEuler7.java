8787efe7b93c0f5dbb46321269112b5609d466e7
Jeremy Marshall <jeremymars@gmail.com>
2016-01-17 20:45:56 -0800

    Cleaned up a bit

--- src/PEuler7/PEuler7.java
+++ src/PEuler7/PEuler7.java
@@ -11,35 +11,39 @@ import java.util.ArrayList;
  *
  * Created by jeremy on 1/17/16.
  */
+
 public class PEuler7 {
+
     public static void main(String[] args) {
 
         System.out.println(findSpecificPrime(10001));
     }
 
     public static double findSpecificPrime(int index) {
-        ArrayList<Double> foundPrimes = new ArrayList<>(index);
-        Boolean prime = true;
+        ArrayList<Double> foundPrimes = new ArrayList<>();
+        Boolean isPrime = true;
+
         foundPrimes.add(2D);
         foundPrimes.add(3D);
 
-        double x;
-
-        for (x = 5D; foundPrimes.size() < index; x += 2D) {
+        for (double x = 5D; foundPrimes.size() < index; x += 2D) {
 
             for (double p : foundPrimes) {
                 if (x % p == 0) {
-                    prime = false;
+                    isPrime = false;
                     break;
                 }
             }
-            if (prime) {
+
+            if (isPrime) {
                 foundPrimes.add(x);
             }
-            System.out.println(prime);
-            prime = true;
+
+            isPrime = true;
+
         }
 
         return foundPrimes.get(index-1);
+
     }
 }
