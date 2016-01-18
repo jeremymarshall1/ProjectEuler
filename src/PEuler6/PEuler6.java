53ad7b48fb051ff8c4a7dabfe9c4d9a3d594a4d7
Jeremy Marshall <jeremymars@gmail.com>
2016-01-17 19:07:06 -0800

    Solved example

--- src/PEuler6/PEuler6.java
+++ src/PEuler6/PEuler6.java
@@ -1,14 +1,16 @@
 package PEuler6;
 
+import java.lang.reflect.Array;
+
 /**
  * Sum square difference
  * Problem 6
  * The sum of the squares of the first ten natural numbers is,
  *
- * 12 + 22 + ... + 102 = 385
+ * 1^2 + 2^2 + ... + 10^2 = 385
  * The square of the sum of the first ten natural numbers is,
  *
- * (1 + 2 + ... + 10)2 = 552 = 3025
+ * (1 + 2 + ... + 10)^2 = 552 = 3025
  * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
  *
  * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
@@ -16,4 +18,27 @@ package PEuler6;
  * Created by jeremy on 1/17/16.
  */
 public class PEuler6 {
+    public static int getSumSqDiff(int num) {
+        int sumSquare = 0;
+        int squareSum = 0;
+        int diff = 0;
+
+        for (int x = 1; x <= num; x++) {
+            sumSquare += x * x;
+            squareSum += x;
+        }
+
+        squareSum *= squareSum;
+
+        if (squareSum > sumSquare) {
+            squareSum *= -1;
+            sumSquare *= -1;
+        }
+
+        return (sumSquare - squareSum);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSumSqDiff(10));
+    }
 }
