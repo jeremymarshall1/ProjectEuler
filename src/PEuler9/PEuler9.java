eaf8d4b7328420d516af9a994532a3a40dd25027
Jeremy Marshall <jeremymars@gmail.com>
2016-02-27 15:58:15 -0800

    Starting 9th problem

---
+++ src/PEuler9/PEuler9.java
@@ -0,0 +1,33 @@
+package PEuler9;
+
+import java.util.ArrayList;
+
+/**
+ * Special Pythagorean triplet
+ * Problem 9
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ *
+ * a^2 + b^2 = c^2
+ * For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+ *
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ *
+ * Notes:
+ *      If a + b + c = 1000, then c = 1000 - a - b
+ *      a^2 + b^2 = (1000 - a - b)^2
+ *      a + b = 1000 - a - b
+ *      2a + 2b = 1000
+ *      a + b = 500
+ *      Therefore, c = 500
+ *      Simplified, in a Pythagorean triplet c will always be 1/2 of the sum
+ *
+ * Created by jeremy on 2/27/16.
+ */
+public class PEuler9 {
+    public static void main(String[] args) {
+
+
+    }
+
+}
