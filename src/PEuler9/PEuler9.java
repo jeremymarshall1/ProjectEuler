5482a7d57848e2019dcffc7562cf151f5965b5dd
Jeremy Marshall <jeremymars@gmail.com>
2016-02-27 18:26:12 -0800

    Removing incorrect work

--- src/PEuler9/PEuler9.java
+++ src/PEuler9/PEuler9.java
@@ -13,21 +13,26 @@ import java.util.ArrayList;
  * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
  * Find the product abc.
  *
- * Notes:
- *      If a + b + c = 1000, then c = 1000 - a - b
- *      a^2 + b^2 = (1000 - a - b)^2
- *      a + b = 1000 - a - b
- *      2a + 2b = 1000
- *      a + b = 500
- *      Therefore, c = 500
- *      Simplified, in a Pythagorean triplet c will always be 1/2 of the sum
  *
  * Created by jeremy on 2/27/16.
  */
 public class PEuler9 {
     public static void main(String[] args) {
+        int [] triplet = findPythagoranTriplet(1000);
+        for (int x: triplet) {
+            System.out.println(x);
+        }
 
+    }
 
+    private static int[] findPythagoranTriplet(int sum) {
+        int [] triplet = new int [3];
+
+        triplet[0] = 0;
+        triplet[1] = 0;
+        triplet[2] = 0;
+
+        return triplet;
     }
 
 }
