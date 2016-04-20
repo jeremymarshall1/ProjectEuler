9ac723309209021d9cfed6de019fb4e53121ff0a
Jeremy Marshall <jeremymars@gmail.com>
2016-04-20 15:05:05 -0700

    Handling command-line arguments

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,39 @@ class Main {
     public static void main(String[] args) {
         Solution test;
         String className;
+        Object tests;
+        boolean single = false;
 
-        if (args.length == 0) {
-            System.out.println("null args");
+        if (args.length > 0) {
+            System.out.println("Running PEuler" + args[0] + ": ");
+            className = "PEuler" + args[0];
+            String[] argsTemp = new String[args.length - 1];
+            for (int x  = 0; x < argsTemp.length; x++) {
+                argsTemp[x] = args[x+1];
+            }
+            args = argsTemp;
+            single = true;
         }
 
         try {
             for (int x = 1;; x++){
                 className = "PEuler" + x;
-                Object tests = Class.forName(className).newInstance();
+                tests = Class.forName(className).newInstance();
                 System.out.print(tests.getClass().getName() + ": ");
                 tests.getClass().getMethod("solve",String[].class).invoke(tests, new Object[]{args});
+                if (single) {
+                    break;
+                }
+
             }
         } catch (Exception err) {
-            System.out.println(err);
-            System.out.println("There are no more tests");
+            if (err.getClass().getName() == "java.lang.ClassNotFoundException") {
+                System.out.println("There are no more tests");
+            } else {
+                System.out.println(err);
+            }
+
+
         }
     }
 }
